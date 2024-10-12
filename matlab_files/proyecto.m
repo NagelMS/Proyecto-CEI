b8dd3ceee25a8a8205584b403f8f58b25d906a3c
NagelMS <nagelmese@gmail.com>
2024-10-11 21:47:04 -0600

    FDM Implementado en Simulink

--- matlab_files/proyecto.m
+++ matlab_files/proyecto.m
@@ -1,4 +1,15 @@
-# Workspace: projectCE1
-# Grupo: 8
-# Integrantes:  Fabian Gomez Quesada
-#               Nagel Mejía Segura 
+% Workspace: projectCE1
+% Grupo: 8
+% Integrantes:  Fabian Gomez Quesada
+%               Nagel Mejía Segura
+
+% Fs: Sample Frequency
+Fs = 100e6;
+% fsz: Frame size
+fsz = 5000;
+
+% f1-f2-f3: Frequency carrier
+f1_AM = 140000;
+f2_AM = 180000;
+f3_AM = 220000;
+
