4a2590d44d57c8aa1d4e3f4764ca3bd4cc925432
NagelMS <nagelmese@gmail.com>
2024-10-12 20:16:30 -0600

    Modificaciones en Proyecto

--- matlab_files/proyecto.m
+++ matlab_files/proyecto.m
@@ -4,23 +4,30 @@
 %               Nagel Mejía Segura
 
 % Fs: Sample Frequency
-Fs = 100e6;
-% fsz: Frame size
-fsz = 5000;
+Fs1 = 500e3;
 
 % f1-f2-f3: Frequency carrier
 f1_AM = 140000;
 f2_AM = 180000;
 f3_AM = 220000;
 
+% Kf: Frecuencia Sensitivity
+Kf = 72e3;
+fc_FM = 1e6;
+
 modelName = 'projectCE1';
 
 open_system(modelName);
 
 set_param([modelName, '/FDM/Spectrum Analyzer'], 'RBWSource', 'property');
-set_param([modelName, '/FDM/Spectrum Analyzer'], 'RBW', '50'); % Set RBW to 1 kHz
+set_param([modelName, '/FDM/Spectrum Analyzer'], 'RBW', '50'); 
 set_param([modelName, '/FDM/Spectrum Analyzer'], 'SampleRate', '500000');
-set_param([modelName, '/FDM/Spectrum Analyzer'], 'PlotAsTwoSidedSpectrum', 'off');
+
+
+%set_param([modelName, '/FM/Spectrum Analyzer'], 'RBWSource', 'property');
+%set_param([modelName, '/FM/Spectrum Analyzer'], 'RBW', '100'); 
+%set_param([modelName, '/FM/Spectrum Analyzer'], 'SampleRate', '500000');
+%set_param([modelName, '/FM/Spectrum Analyzer'], 'PlotAsTwoSidedSpectrum', 'off');
 
 %scope = spectrumAnalyzer(SampleRate=Fs, AveragingMethod="exponential", RBWSource="property",RBW=500);
 
