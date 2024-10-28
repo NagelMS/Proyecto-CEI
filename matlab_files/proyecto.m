68350297484eba6a2d882ca47d199816285be7ba
NagelMS <nagelmese@gmail.com>
2024-10-27 19:57:28 -0600

    Continuación receptor superheterodino

--- matlab_files/proyecto.m
+++ matlab_files/proyecto.m
@@ -10,13 +10,12 @@ f2_AM = 60e3;
 f3_AM = 100e3;
 
 % Kf: Frecuencia Sensitivity
-Kf = 72e3;
+Kf = 100e3;
 fc_FM = 1e6;
 
 
 % Fs: Sample Rate
-Fs1 = 500e3;
-Fs2 = 4*fc_FM;
+Fs = 4*fc_FM;
 
 % If: Frecuencia Intermedia
 If = 500e3;
@@ -25,11 +24,3 @@ If = 500e3;
 modelName = 'projectCE1';
 
 open_system(modelName);
-
-set_param([modelName, '/FDM/Spectrum Analyzer'], 'RBWSource', 'property');
-set_param([modelName, '/FDM/Spectrum Analyzer'], 'RBW', '50');
-%set_param([modelName, '/FDM/Spectrum Analyzer'], 'FrequencyScale', 'Linear');
-
-
-%set_param([modelName, '/FM-Dem/Spectrum Analyzer'], 'SampleRate', '4e6');
-%set_param([modelName, '/FM/Spectrum Analyzer'], 'SampleRate', '4e6');
